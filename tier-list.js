40cf897826d6d34cbbf55233ef133714853f218e
leejustin01 <justlee0308@gmail.com>
2025-03-12 16:13:11 -0700

    Added cors to allow calls from main program

--- tier-list.js
+++ tier-list.js
@@ -1,7 +1,11 @@
 const express = require('express');
+const cors = requrie('cors');
 
 const PORT = 8082;
 const app = express();
+app.use(cors({
+    origin: 'http://localhost:3000'
+}))
 
 app.get('/brawler', (req, res) => {
     res.status(200).send("/brawler_tier_list.png");
